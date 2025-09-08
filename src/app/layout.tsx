9deefeab9b1764363e1bf5261cf2acf317f32575
Javier Velasquez <xavi7x@gmail.com>
2025-09-08 23:29:22 +0000

    quiero que tenga tema oscuro por defecto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
         />
       </head>
       <body className="font-body antialiased">
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
           {children}
           <Toaster />
         </ThemeProvider>
