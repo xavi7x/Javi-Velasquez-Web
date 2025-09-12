e0742addb7d0123375ba1f1c081969687c196a24
Javier Velasquez <xavi7x@gmail.com>
2025-09-12 01:51:22 +0000

    quiero que las esquinas sean mas redondeadas (_for element img_)

--- src/components/home/PortfolioGrid.tsx
+++ src/components/home/PortfolioGrid.tsx
@@ -16,7 +16,7 @@ export function PortfolioGrid() {
               href={`/portfolio/${project.slug}`}
               className="group block"
             >
-              <div className="overflow-hidden rounded-lg">
+              <div className="overflow-hidden rounded-2xl">
                 <Image
                   src={project.thumbnail}
                   width={600}
