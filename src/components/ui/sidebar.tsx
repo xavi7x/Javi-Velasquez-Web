87533014f9cb9a9fa5058c539d8cddddfd954be2
Javier Velasquez <xavi7x@gmail.com>
2025-11-19 23:18:30 +0000

    no entiendo porque esto no ocupa el ancho completo de la pagina (_for el

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -141,7 +141,7 @@ const SidebarProvider = React.forwardRef<
               } as React.CSSProperties
             }
             className={cn(
-              "group/sidebar-wrapper flex min-h-svh has-[[data-variant=inset]]:bg-sidebar has-[[data-variant=floating]]:bg-muted/30",
+              "group/sidebar-wrapper flex min-h-svh w-full",
               className
             )}
             ref={ref}
@@ -324,7 +324,7 @@ const SidebarInset = React.forwardRef<
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background",
         "md:peer-data-[variant=inset]:m-2 md:peer-data-[variant=inset]:rounded-lg",
-        "md:peer-data-[variant=floating]:my-2 md:peer-data-[variant=floating]:mr-2 md:peer-data-[variant=floating]:rounded-lg",
+        "md:peer-data-[variant=floating]:my-2 md:peer-data-[side=left]:peer-data-[variant=floating]:mr-2 md:peer-data-[side=right]:peer-data-[variant=floating]:ml-2 md:peer-data-[variant=floating]:rounded-lg",
         className
       )}
       {...props}
@@ -374,7 +374,7 @@ const SidebarFooter = React.forwardRef<
     <div
       ref={ref}
       data-sidebar="footer"
-      className={cn("flex flex-col gap-2 p-2", className)}
+      className={cn("flex flex-col gap-2 p-2 mt-auto", className)}
       {...props}
     />
   )
@@ -632,9 +632,8 @@ const SidebarMenuBadge = React.forwardRef<
     ref={ref}
     data-sidebar="menu-badge"
     className={cn(
-      "absolute right-2 flex size-5 items-center justify-center rounded-full text-xs font-medium",
+      "absolute right-2 top-1.5 flex h-5 w-5 items-center justify-center rounded-full text-xs font-medium",
       "peer-data-[size=sm]/menu-button:top-1",
-      "peer-data-[size=default]/menu-button:top-1.5",
       "peer-data-[size=lg]/menu-button:top-2.5",
       "group-data-[collapsible=icon]:hidden",
       className
