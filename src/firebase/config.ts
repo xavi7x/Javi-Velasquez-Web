8bbff6ff12f823a3f55fb0888a03f0eef402dc8a
Javier Velasquez <xavi7x@gmail.com>
2025-11-19 14:29:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "velsquez-digital",
+  "appId": "1:756479849375:web:08c8fd2feadc9fa4f243aa",
+  "apiKey": "AIzaSyC5Z-W2Bufk2IStfoU_a-BY1MfOOGPBZNk",
+  "authDomain": "velsquez-digital.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "756479849375"
+};
