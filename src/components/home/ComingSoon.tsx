4ad1d97e9949daceacb148561389524079a4cc4b
Javier Velasquez <xavi7x@gmail.com>
2025-11-20 16:01:44 +0000

    el modo construccion quita el header, dejando solo el logo una etiqueta

--- src/components/home/ComingSoon.tsx
+++ src/components/home/ComingSoon.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { Header } from '@/components/shared/Header';
+import { MinimalHeader } from '@/components/shared/MinimalHeader';
 import { CursorGradientWrapper } from '@/components/shared/CursorGradientWrapper';
 import { Hero } from '@/components/home/Hero';
 
 export function ComingSoon() {
   return (
     <CursorGradientWrapper>
-      <Header />
+      <MinimalHeader />
       <main className="flex-1">
         <Hero />
       </main>
