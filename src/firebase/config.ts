affcd2ac3a271dcb6c23cf6fd4b8d0005088440c
Javier Velasquez <xavi7x@gmail.com>
2025-11-20 14:21:26 +0000

    creo que el error es que no se esta usando el enlace corr4cto del bucket

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   "appId": "1:756479849375:web:08c8fd2feadc9fa4f243aa",
   "apiKey": "AIzaSyC5Z-W2Bufk2IStfoU_a-BY1MfOOGPBZNk",
   "authDomain": "velsquez-digital.firebaseapp.com",
-  "storageBucket": "velsquez-digital.appspot.com",
+  "storageBucket": "velsquez-digital.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "756479849375"
 };
