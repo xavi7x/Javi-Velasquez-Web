f430c603f6eb1860e21083fcf3bf782ea7b12a02
Javier Velasquez <xavi7x@gmail.com>
2025-11-24 21:53:26 +0000

    Try fixing this error: `Console Error: FirebaseError: The query requires

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -24,7 +24,7 @@ function HomePageContent() {
   
   const { data: projectsData, isLoading: areProjectsLoading } = usePublicCollection<Project>(
     'projects',
-    [where('isPublic', '==', true), orderBy('order', 'asc')]
+    [where('isPublic', '==', true)]
   );
 
   if (isSettingsLoading) {
